13fcd41f0efcc06b21d753a2141177f5bdf20de2
Pisa2023 <146504797+Pisa2023@users.noreply.github.com>
2024-12-15 15:34:29 +0800

    Update dbcon.php

--- client/back-office/security/dbcon.php
+++ client/back-office/security/dbcon.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$username = "u741634386_PISAproto";
-$password = "Pisaproto2023";
-$database = "u741634386_PISA";
+$username = "u741634386_lmsproto";
+$password = "d9|4QftMf5T";
+$database = "u741634386_pisaproto";
  
 $con = mysqli_connect($host, $username, $password, $database);
 
